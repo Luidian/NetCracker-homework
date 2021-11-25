d138c3807674a4cd01b17d57487252902fd77967
Luidian <spaskinsaha@gmail.com>
2021-11-25 15:18:30 +0300

    testing the search method

--- src/test/java/com/example/RepositoryTest.java
+++ src/test/java/com/example/RepositoryTest.java
@@ -1,22 +1,26 @@
 package com.example;
 
+import com.example.contracts.Contract;
 import com.example.contracts.DTVContract;
 import com.example.contracts.MCContract;
 import com.example.contracts.WIContract;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RepositoryTest {
 
     Human h1 = new Human(0, "fn", "n", "mn", LocalDate.of(1234, 5, 6), "m");
-    WIContract wi1 = new WIContract(1, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 101392, h1,100);
-    WIContract wi2 = new WIContract(2, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 101392, h1,100);
-    WIContract wi3 = new WIContract(3, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 101392, h1,100);
-    DTVContract dtv = new DTVContract(1, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 101392, h1, "all");
-    MCContract mc = new MCContract(1, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 101392, h1, 200, 500, 5000);
+    Human h2 = new Human(1, "fn1", "n1", "mn1", LocalDate.of(1235, 6, 7), "m");
+    WIContract wi1 = new WIContract(1, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 5, h1,100);
+    WIContract wi2 = new WIContract(2, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 55, h2,80);
+    WIContract wi4 = new WIContract(2, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 555, h2,50);
+    WIContract wi3 = new WIContract(3, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 5555, h1,20);
+    DTVContract dtv = new DTVContract(1, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 111, h2, "all");
+    MCContract mc = new MCContract(1, LocalDate.of(1234, 5, 6), LocalDate.of(1234, 6, 6), 222, h1, 200, 500, 5000);
 
     @Test
     void add() {
@@ -64,5 +68,22 @@ class RepositoryTest {
 
         MCContract actualMC = (MCContract) r.getById( 2, mc.getClass());
         assertNull(actualMC);
+    }
+    @Test
+    void search() {
+        Repository r = new Repository();
+        r.add(wi1); r.add(wi2); r.add(wi3); r.add(wi4); r.add(dtv);
+        Optional<Contract> actual = r.search(i -> i.getContractOwner() == h2);
+        WIContract expected = wi2;
+        assertEquals(expected, actual.get());
+
+        Optional<Contract> actual1 = r.search(i -> i.getContractNumber() == 111);
+        DTVContract expected1 = dtv;
+        assertEquals(expected1, actual1.get());
+
+        Optional<Contract> actual3 = r.search(i -> i.getId() == 55);
+        Optional<Contract> expected3 = Optional.empty();
+        assertEquals(expected3, actual3);
+
     }
 }
