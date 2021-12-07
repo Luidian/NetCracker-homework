42ddd4b5b5dbd32c7c3fbbd7560c97249eb9f483
Luidian <spaskinsaha@gmail.com>
2021-12-07 16:58:44 +0300

    added comments to CsvParser

--- src/main/java/com/example/Parser/CsvParser.java
+++ src/main/java/com/example/Parser/CsvParser.java
@@ -18,9 +18,19 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-
+/**
+ * CsvParser
+ * @author  Alexanrd Spaskin
+ */
 public class CsvParser {
 
+    /**
+     * Method parses csv file
+     * @param path path to csv file
+     * @param repository the storage to which the new data will be added
+     * @return object of class Repository
+     * @throws CsvParseException
+     */
     public Repository parse(final String path, Repository repository) throws CsvParseException{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.d");
         CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
@@ -69,6 +79,17 @@ public class CsvParser {
         return repository;
     }
 
+    /**
+     * DTVContract add method
+     * @param startDate start date of the contract
+     * @param endDate end date of the contract
+     * @param contractNumber Number of contract
+     * @param contractOwner Owner of contract
+     * @param channelPackage Channel package
+     * @param formatter format of date
+     * @return object of class DTVContract
+     * @throws CsvParseException
+     */
     private DTVContract tv(String startDate, String endDate, String contractNumber, Human contractOwner, String channelPackage, DateTimeFormatter formatter) throws CsvParseException {
         try {
             LocalDate sDate = LocalDate.parse(startDate, formatter);
@@ -80,6 +101,17 @@ public class CsvParser {
         }
     }
 
+    /**
+     * WIContract add method
+     * @param startDate start date of the contract
+     * @param endDate end date of the contract
+     * @param contractNumber Number of contract
+     * @param contractOwner Owner of contract
+     * @param speed speed of internet
+     * @param formatter format of date
+     * @return object of class WIContract
+     * @throws CsvParseException
+     */
     private WIContract internet(String startDate, String endDate, String contractNumber, Human contractOwner, String speed, DateTimeFormatter formatter) throws CsvParseException {
         try {
             LocalDate sDate = LocalDate.parse(startDate, formatter);
@@ -92,6 +124,19 @@ public class CsvParser {
         }
     }
 
+    /**
+     * MCContract add method
+     * @param startDate start date of the contract
+     * @param endDate end date of the contract
+     * @param contractNumber Number of contract
+     * @param contractOwner Owner of contract
+     * @param minutes minutes
+     * @param sms sms
+     * @param traffic traffic
+     * @param formatter format of date
+     * @return object of class MCContract
+     * @throws CsvParseException
+     */
     private MCContract mobile(String startDate, String endDate, String contractNumber, Human contractOwner, String minutes, String sms, String traffic, DateTimeFormatter formatter) throws CsvParseException {
         try {
             LocalDate sDate = LocalDate.parse(startDate, formatter);
@@ -106,6 +151,19 @@ public class CsvParser {
         }
     }
 
+    /**
+     * Method creates an object of the Human class
+     * @param firstName first name of contract owner
+     * @param name name of contract owner
+     * @param middleName middle name of contract owner
+     * @param gender gender of contract owner
+     * @param dateOfBirthday date of birthday of contract owner
+     * @param passportSeries passport series of contract owner
+     * @param passportID passport id of contract owner
+     * @param formatter format of date
+     * @return object of class Human
+     * @throws CsvParseException
+     */
     private Human human(String firstName, String name, String middleName, String gender, String dateOfBirthday, String passportSeries, String passportID, DateTimeFormatter formatter) throws CsvParseException {
         try {
             int passportS = Integer.parseInt(passportSeries);
